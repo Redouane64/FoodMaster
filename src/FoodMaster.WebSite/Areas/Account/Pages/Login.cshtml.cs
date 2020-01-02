9ddde460f7913419d080ac2a20d89029c4179029
Redhouane Sobaihi <s-redouane@outlook.com>
2020-01-02 23:16:56 +0700

    Add project files.

---
+++ src/FoodMaster.WebSite/Areas/Account/Pages/Login.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using FoodMaster.WebSite.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodMaster.WebSite.Areas.Account.Pages
+{
+    [BindProperties]
+    public class LoginModel : PageModel
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var claims = new Claim[]
+            {
+                new Claim(ClaimTypes.Name, FullName),
+                new Claim(ClaimTypes.DateOfBirth, BirthDate.ToString())
+            };
+
+            await HttpContext.SignInAsync(new ClaimsPrincipal(new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme)));
+
+            return RedirectToPagePermanent("Index");
+        }
+
+        public async Task<IActionResult> OnGetSignOutAsync()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToPage();
+        }
+    }
+}
---
+++ src/FoodMaster.WebSite/Models/LoginCredentials.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FoodMaster.WebSite.Models
+{
+    public class LoginCredentials
+    {
+        [Required]
+        [RegularExpression("[a-zA-Z]+\\s[a-zA-Z]+")]
+        public string FullName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+    }
+}
