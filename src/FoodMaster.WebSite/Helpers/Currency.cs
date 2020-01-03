2f8894913283bad905b2e67eb5120cdecfc2594c
Redhouane Sobaihi <s-redouane@outlook.com>
2020-01-03 19:06:04 +0700

    Show price in rubles.

---
+++ src/FoodMaster.WebSite/Helpers/Currency.cs
@@ -0,0 +1,9 @@
+﻿using System.Globalization;
+
+namespace FoodMaster.WebSite.Helpers
+{
+    public static class Currency
+    {
+        public static string AsRubles(decimal value) => value.ToString("C", CultureInfo.GetCultureInfo("ru-ru"));
+    }
+}
