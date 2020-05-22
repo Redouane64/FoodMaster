c8a5b769577e5f0a01630afbaf00820c25ffaa04
Redhouane Sobaihi <s-redouane@outlook.com>
2020-05-22 23:32:31 +0700

    Update identity schema

---
+++ src/FoodMaster.WebSite/Domain/Claim.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodMaster.WebSite.Domain
+{
+    public class Claim
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("value")]
+        public string Value { get; set; }
+
+    }
+}
---
+++ src/FoodMaster.WebSite/Domain/Role.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FoodMaster.WebSite.Domain
+{
+    public class Role
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        public ICollection<UserRole> Users { get; set; }
+    }
+}
