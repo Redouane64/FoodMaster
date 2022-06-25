442e2a2dfb5dfeb7e26075bf97fce00d1d660397
redouane <redouane.sobaihi@live.com>
2022-06-25 19:25:28 +0700

    feat(identity): add identity
    - add db context
    - add necessary nuget packages
    - clean up
    - create identity migrations

--- src/FoodMaster.WebSite/Data/FoodMasterDataContext.cs
+++ src/FoodMaster.WebSite/Data/FoodMasterDataContext.cs
@@ -1,62 +1,15 @@
-﻿
-using FoodMaster.WebSite.Domain;
+﻿using FoodMaster.WebSite.Domain.Customers;
 
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace FoodMaster.WebSite.Data
 {
-    public class FoodMasterDataContext : DbContext
+    public class FoodMasterDataContext : IdentityDbContext<User>
     {
         public FoodMasterDataContext(DbContextOptions<FoodMasterDataContext> options)
             : base(options)
         { }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<User>()
-                .ToTable("users");
-
-            modelBuilder.Entity<Claim>()
-                .ToTable("claims");
-
-            modelBuilder.Entity<Role>()
-                .ToTable("roles");
-
-            modelBuilder.Entity<UserClaim>()
-                .ToTable("user_claims")
-                .HasKey(x => new { x.UserId, x.ClaimId });
-
-            modelBuilder.Entity<UserRole>()
-                .ToTable("user_roles")
-                .HasKey(x => new { x.UserId, x.RoleId });
-
-            modelBuilder.Entity<Cart>()
-                .ToTable("carts");
-
-            modelBuilder.Entity<CartItem>()
-                .ToTable("cart_items");
-
-            modelBuilder.Entity<Category>()
-                .ToTable("categories");
-
-            modelBuilder.Entity<Menu>()
-                .ToTable("menus");
-
-            modelBuilder.Entity<MealIngredient>()
-                .ToTable("meal_ingredients");
-
-            modelBuilder.Entity<Meal>()
-                .ToTable("meals");
-
-            modelBuilder.Entity<Ingredient>()
-                .ToTable("ingredients");
-
-            modelBuilder.Entity<Order>()
-                .ToTable("orders");
-
-            modelBuilder.Entity<OrderItem>()
-                .ToTable("order_items");
-
-        }
     }
 }
--- src/FoodMaster.WebSite/Startup.cs
+++ src/FoodMaster.WebSite/Startup.cs
@@ -37,60 +37,10 @@ namespace FoodMaster.WebSite
         {
             services.AddRazorPages();
             services.AddControllers();
-
-            services.AddMediatR(typeof(Startup));
-
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-                    .AddCookie(options => {
-                        options.LoginPath = "/Account/GuestLogin";
-                        options.LogoutPath = "/Account/GuestLogin?handler=SignOut";
-                        options.ClaimsIssuer = "FoodMaster";
-                        options.Cookie.MaxAge = TimeSpan.FromDays(5);
-                        options.Cookie.IsEssential = true;
-                        options.Cookie.Name = "FoodMaster";
-                        options.EventsType = typeof(CookieValidatorEvents);
-                    });
-            services.AddScoped<CookieValidatorEvents>();
-
-            services.AddAutoMapper(this.GetType().Assembly);
             
-            services.AddSingleton<JsonDataContext>(_ => JsonDataContext.Create());
-            services.AddSingleton(s => s.GetRequiredService<JsonDataContext>().Meals);
-            services.AddSingleton(s => s.GetRequiredService<JsonDataContext>().Ingredients);
-            services.AddSingleton(s => s.GetRequiredService<JsonDataContext>().Users);
-            services.AddSingleton(s => s.GetRequiredService<JsonDataContext>().Orders);
-            services.AddSingleton(s => s.GetRequiredService<JsonDataContext>().Categories);
-
-            services.AddScoped<IStockService, StockService>();
-            services.AddScoped<IMealsService, MealsService>();
-            services.AddScoped<IUsersService, UsersService>();
-            services.AddScoped<IOrdersService, OrdersService>();
-
-            services.AddHttpContextAccessor();
-            services.AddScoped<ICartService>(s =>
-            {
-                var carts = s.GetRequiredService<JsonDataContext>().Carts;
-                var httpContextAccessor = s.GetRequiredService<IHttpContextAccessor>();
-                var userId = httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-                var userCart = carts.FirstOrDefault(cart => cart.UserId == userId);
-
-                if (userCart == null)
-                {
-                    userCart = new Cart { UserId = userId, Items = new List<CartItem>() };
-                    carts.Add(userCart);
-                }
-
-                var mealService = s.GetRequiredService<IMealsService>();
-                return new CartService(userCart.Items, mealService);
-            });
-
-            services.AddSingleton<WriteToDiskFilterAttribute>();
-            services.AddSingleton<IDiscountProvider, BirthdayDiscountProvider>();
-
             services.AddDbContext<FoodMasterDataContext>(options =>
             {
-                options.UseNpgsql(Configuration.GetConnectionString("Default"));
+                options.UseSqlite(Configuration.GetConnectionString("Default"));
             });
         }
 
