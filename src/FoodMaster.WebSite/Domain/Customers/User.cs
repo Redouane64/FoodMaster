781e3cd0055c0373758ac140fdf45f5b50be4c78
redouane <redouane.sobaihi@live.com>
2022-06-25 19:25:28 +0700

    feat(identity): add identity
    - add db context
    - add necessary nuget packages
    - clean up
    - create identity migrations

---
+++ src/FoodMaster.WebSite/Domain/Customers/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Identity;
+
+namespace FoodMaster.WebSite.Domain.Customers
+{
+    public class User : IdentityUser
+    {
+        public string FullName { get; set; }
+        
+        public DateTime BirthDate { get; set; }
+    }
+}
